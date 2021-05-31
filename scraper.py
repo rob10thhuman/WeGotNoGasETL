8f0e95b416fc82cddf26acd26b5b1ed9d9a5b1fc
Rob Thompson <macpro@Robs-Mac-Pro.local>
2021-05-31 11:01:50 -0600

    scaper notebook updated

--- scraper.py
+++ scraper.py
@@ -6,9 +6,7 @@ import pandas as pd
 import json 
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
-
 driver.get('https://www.caranddriver.com/shopping-advice/g32463239/new-ev-models-us/')
-
 model = driver.find_elements_by_class_name('listicle-slide-hed-text')
 traits = driver.find_elements_by_class_name('listicle-slide-dek ul')
 
@@ -27,15 +25,38 @@ cars_list = pd.DataFrame(cars_list)
 traits_list = pd.DataFrame(traits_list)
 
 cars_list['Traits'] = traits_list
+cars_list = cars_list.rename(columns={0: 'Type', 'Traits': 'Data'})
 
-print(cars_list)
+cars_list
+
+cars_list['Data'][6] = cars_list['Data'][7]
+cars_list['Data'][7] = cars_list['Data'][8]
+cars_list['Data'][8] = cars_list['Data'][9]
+cars_list['Data'][9] = cars_list['Data'][10]
+cars_list['Data'][10] = cars_list['Data'][11]
+cars_list['Data'][11] = cars_list['Data'][12]
+cars_list['Data'][12] = cars_list['Data'][13]
+cars_list['Data'][13] = cars_list['Data'][14]
+cars_list['Data'][14] = cars_list['Data'][15]
+cars_list['Data'][15] = cars_list['Data'][16]
+cars_list['Data'][16] = cars_list['Data'][17]
+cars_list['Data'][17] = cars_list['Data'][18]
+cars_list['Data'][18] = cars_list['Data'][19]
+cars_list = cars_list.drop(19)
+
+cars_list = cars_list.set_index('Type')
+
+cars_list
 
 result = cars_list.to_json(orient = 'split')
+result
+
 parsed = json.loads(result)
+print(parsed)
 
 joutput = json.dumps(parsed, indent=4)
 print(joutput)
 
-f = open("cars_list.csv", "a")
-f.write(cars_list)
+f = open("jsonify.txt", "a")
+f.write(joutput)
 f.close()
